049e381f45bec1ae9af6bca23024ecb50c6d5ca0
Henk Boom <henk@henk.ca>
2013-01-26 21:01:36 -0500

    up

--- Assets/Scripts/Sprite.cs
+++ Assets/Scripts/Sprite.cs
@@ -4,8 +4,11 @@ using System.Collections;
 [ExecuteInEditMode]
 public class Sprite : MonoBehaviour
 {
+    public Vector3 up = Vector3.up;
+
 	void Update ()
     {
-        transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
+        transform.rotation =
+            Quaternion.LookRotation(-Camera.main.transform.forward, up);
 	}
 }
