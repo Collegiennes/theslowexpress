5308e3411200009b55cf42e6f3de9f3574728491
Renaud Bédard <renaud.bedard@gmail.com>
2013-01-27 02:17:22 -0500

    Terrain grid!

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -7,5 +7,13 @@ public class Background : MonoBehaviour
     {
         Vector3 cameraPosition = Camera.main.transform.position;
 	    transform.position = new Vector3(cameraPosition.x, 0, cameraPosition.z);
+
+        Vector2 scaleXZ = new Vector2(transform.localScale.x, transform.localScale.z);
+        scaleXZ *= 10;
+
+        Vector2 positionXZ = new Vector2(cameraPosition.x, cameraPosition.z);
+        positionXZ = Vector2.Scale(positionXZ, new Vector2(1 / scaleXZ.x, 1 / scaleXZ.y));
+
+        transform.FindChild("Ground").renderer.material.SetTextureOffset("_GridTex", positionXZ);
 	}
 }
