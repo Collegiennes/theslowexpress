962b6b2a9e4fb467e2637d222f13477a7f2730fa
Renaud Bédard <renaud.bedard@gmail.com>
2013-01-27 02:31:10 -0500

    Broken grid movement but it looks cool

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -12,6 +12,11 @@ public class Background : MonoBehaviour
         scaleXZ *= 10;
 
         Vector2 positionXZ = new Vector2(cameraPosition.x, cameraPosition.z);
+
+        float additionalScale = cameraPosition.y;
+        transform.FindChild("Ground").localScale = new Vector3(32 + additionalScale, 32 + additionalScale, 32 + additionalScale);
+        transform.FindChild("Ground").localPosition = new Vector3(0, 0, 125 + additionalScale * 10);
+
         positionXZ = Vector2.Scale(positionXZ, new Vector2(1 / scaleXZ.x, 1 / scaleXZ.y));
 
         transform.FindChild("Ground").renderer.material.SetTextureOffset("_GridTex", positionXZ);
