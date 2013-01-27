dd81db7d58e2a46ff052a831f67a6978590ab96a
Henk Boom <henk@henk.ca>
2013-01-27 01:51:50 -0500

    some trees and such

--- Assets/Scripts/SpawnObstacles.cs
+++ Assets/Scripts/SpawnObstacles.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnObstacles : MonoBehaviour
 {
-    public Transform obstaclePrefab;
+    public List<Transform> obstaclePrefabs;
 
 	void Start ()
     {
@@ -12,11 +12,22 @@ public class SpawnObstacles : MonoBehaviour
 	
     void SpawnRegion()
     {
-        for(int i = 0; i < 1000; i++)
+        for(int i = -20; i <= 20; i++)
+        for(int j = 0; j < 50; j++)
         {
-            Instantiate(obstaclePrefab,
-                new Vector3(Random.value-0.5f, 0, Random.value) * 1000,
-                Quaternion.identity);
+            float r = PoisedNoise.FourOctaveHash(i+1000, j+100000);
+            r = r * r;
+            int treesToSpawn = Mathf.FloorToInt(r * 10);
+            //treesToSpawn += (j-50)/10;
+
+            for(int t = 0; t < treesToSpawn; t++)
+            {
+                Transform prefab = obstaclePrefabs[Mathf.FloorToInt(
+                    PoisedNoise.UintToFloat(PoisedNoise.Hash((uint)i, (uint)j, (uint)t))*obstaclePrefabs.Count)];
+                Instantiate(prefab,
+                    new Vector3(i+Random.value, 0, j+Random.value) * 30,
+                    Quaternion.identity);
+            }
         }
     }
 }
